0f26a225fcb8bd5ab7c5e74d48e8e296c4a3b7da
Simon Hosie <simonhosie77+github@gmail.com>
2016-11-20 21:34:21 -0800

    update

--- source/main.cpp
+++ source/main.cpp
@@ -19,12 +19,18 @@ int main()
 {
     qdec.start();
      
+    int oldx = qdec.getPosition();
     for (;;)
     {
         int x = qdec.getPosition();
-        int v = qdec.getSpeed(720, 60 * 1000000);
+        int v = qdec.getSpeed();
         printf("%6d  %6d  \r", x, v);
         fflush(stdout);
         wait_ms(51);
+
+        /* add a wee snooze here to see what happens when we don't poll */
+        if (oldx < 720 && x >= 720)
+            wait_ms(3000);
+        oldx = x;
     }
 }
